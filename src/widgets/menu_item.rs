b969132d1fe64db03d1843b6a3d2f56629b18d91
devZu9 <zumas@yandex.ru>
2026-07-18 12:04:07 +0300

    debug: MenuItem log только по клику

--- src/widgets/menu_item.rs
+++ src/widgets/menu_item.rs
@@ -80,13 +80,6 @@ pub fn render(ui: &mut egui::Ui, node: &serde_json::Value, ctx: &mut RenderCtx) 
         &ctx.inherited,
     );
 
-    log::debug!("MenuItem '{}' | pad: [{}, {}, {}, {}] | size: {:.0}x{:.0} (inner: {:.0}x{:.0})",
-        text,
-        pad.top, pad.right, pad.bottom, pad.left,
-        out.response.rect.width(), out.response.rect.height(),
-        out.inner_rect.width(), out.inner_rect.height(),
-    );
-
     let text_x = match align.as_str() {
         "center" => egui::Align::Center.align_size_within_range(csize.x, out.inner_rect.x_range()).min,
         "right"  => out.inner_rect.right() - csize.x,
@@ -96,6 +89,12 @@ pub fn render(ui: &mut egui::Ui, node: &serde_json::Value, ctx: &mut RenderCtx) 
     ui.painter().galley_with_override_text_color(egui::pos2(text_x, text_y), galley, color_icon);
 
     if out.response.clicked() && enabled {
+        log::debug!("MenuItem '{}' | pad: [{}, {}, {}, {}] | size: {:.0}x{:.0} (inner: {:.0}x{:.0})",
+            text,
+            pad.top, pad.right, pad.bottom, pad.left,
+            out.response.rect.width(), out.response.rect.height(),
+            out.inner_rect.width(), out.inner_rect.height(),
+        );
         if let Some(action_name) = action {
             let mut action_ctx = crate::actions::ActionCtx::new()
                 .with_target(target.unwrap_or(""))
